302e5f520310fb98f9bdd7e6fdc983eb61edce22
Jared Brewer <jaredbrewer@me.com>
2022-12-08 20:06:41 -0500

    Added all of the Illustrator files to generate the figures. Hoping it isn't too big.

--- source/citesorter.py
+++ source/citesorter.py
@@ -1,19 +1,27 @@
 import re
 
-with open("intro.tex", "r") as file:
-	text = file.read()
+def citesorter(tex):
+	with open(tex, "r") as file:
+		text = file.read()
 
-cites = re.findall(r"\\citep{.*?}", text)
+	cites = re.findall(r"\\citep{.*?}", text)
+	cite_dict = {}
 
-for cite in cites:
-	cite_strip = cite.replace("\citep{", "").replace("}", "").replace(" ", "")
-	cite_split = cite_strip.split(",")
-	cite_split.sort(key = lambda x: int("".join(filter(str.isdigit, x))))
-	cite_str = "".join(["\citep{", ", ".join(cite_split), "}"])
-	cite_dict[cite] = cite_str
+	for cite in cites:
+		cite_strip = cite.replace("\citep{", "").replace("}", "").replace(" ", "")
+		cite_split = cite_strip.split(",")
+		hasnums = []
+		for cs in cite_split:
+			nums = [char.isdigit() for char in cs]
+			if True not in nums:
+				hasnums.append(False)
+		if hasnums:
+			cite_split.sort(key = lambda x: int("".join(filter(str.isdigit, x))))
+			cite_str = "".join(["\citep{", ", ".join(cite_split), "}"])
+			cite_dict[cite] = cite_str
 
-for key, value in cite_dict.items():
-	text = text.replace(key, value)
+	for key, value in cite_dict.items():
+		text = text.replace(key, value)
 
-with open("new_intro.tex", "w") as output:
-	output.write(text)
+	with open(tex, "w") as output:
+		output.write(text)
